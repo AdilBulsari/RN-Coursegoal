885d5b22b99a5d0ad491e718ac1d5fa23cc257c2
AdilBulsari <adilbulsari@gmail.com>
2022-10-23 00:28:36 +0100

    added flexbox layout for text, buttons and list

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your Course Goal..." style={styles.textInput} />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "red",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 6,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
